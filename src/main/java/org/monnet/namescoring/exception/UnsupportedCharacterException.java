1ec0cfbe6bcb1d613ba6188fcad02027d89bacd3
Alex Monnet <alexcmonnet@gmail.com>
2020-04-12 22:36:46 -0500

    Removed unused dependency and lombok getter

--- src/main/java/org/monnet/namescoring/exception/UnsupportedCharacterException.java
+++ src/main/java/org/monnet/namescoring/exception/UnsupportedCharacterException.java
@@ -2,19 +2,14 @@ package org.monnet.namescoring.exception;
 
 import org.monnet.namescoring.entity.CharacterScoreMap;
 
-import lombok.Getter;
-
 /**
  * This exception expresses that the application was given an unsupported character to score. Either
  *  implement the character in a score map {@link CharacterScoreMap} or alert the user to the un-supported character. 
  */
-@Getter
 public class UnsupportedCharacterException extends Exception {
 
     private static final long serialVersionUID = 4509242987139994938L;
     
-	private String unsupportedCharacter;
-
     public UnsupportedCharacterException(char unsupportedCharacter) {
         super("The following character is not supported: " + unsupportedCharacter);
     }
--- src/main/java/org/monnet/namescoring/service/implmentation/FirstNameScoringServiceImpl.java
+++ src/main/java/org/monnet/namescoring/service/implmentation/FirstNameScoringServiceImpl.java
@@ -54,7 +54,4 @@ public class FirstNameScoringServiceImpl implements ScoringService {
         
 		return runningTally;
 	}
-
-    
-
 }
